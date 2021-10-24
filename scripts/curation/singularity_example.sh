99e68a40b7ed638383363e31eb96542bc2620408
Ellyn Butler <ellynrbutler@gmail.com>
2021-10-24 17:35:38 -0500

    first singularity call fixed

--- scripts/curation/singularity_example.sh
+++ scripts/curation/singularity_example.sh
@@ -1,13 +1,15 @@
-singularity shell --writable-tmpfs --cleanenv \
+singularity pull docker://nipy/heudiconv:0.9.0
+
+ingularity shell --writable-tmpfs --cleanenv \
   -B /projects/b1108/data/BIDS_factory/Temple/data_dump/Alloy_RISE:/base \
   /home/erb9722/heudiconv_0.9.0.sif
 
-singularity exec --writable-tmpfs --cleanenv \
+singularity run --writable-tmpfs --cleanenv \
   -B /projects/b1108/data/BIDS_factory/Temple/data_dump/Alloy_RISE:/base \
-  /home/erb9722/heudiconv_0.9.0.sif -d /base/Dicom/sub-{subject}/[0-9]/*.dcm \
+  /home/erb9722/heudiconv_0.9.0.sif -d /base/Dicom/sub-{subject}/*/*.dcm \
   -o /base/Nifti/ -f convertall -s 991 -c none --overwrite
 
-# October 12, 2021: Have to figure out how to get heudiconv to ignore junk directory
 #https://reproducibility.stanford.edu/bids-tutorial-series-part-2a/#heuman3
 
 # October 12, 2021: Line 8 isn't working (-d flag)
+# October 24, 2021: 0.9.0 works when running from within the container... odd
