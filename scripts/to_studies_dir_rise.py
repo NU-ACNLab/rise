d2d0c3f74861ac6288aafadce16071489b72579d
Kat Seitz <katharina.seitz@northwestern.edu>
2022-11-08 09:48:26 -0600

    to studies dir script rise

---
+++ scripts/to_studies_dir_rise.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import os
+import shutil
+
+'''
+    Copies foundations participants from /data/RISE to /studies
+            Parameters:
+                    none
+            Returns:
+                    none
+'''
+def mover():
+    work_dir = "/projects/b1108/data/RISE/"
+    for partic in os.listdir(work_dir):
+        if(partic[0:4] == "sub-"):
+            wd = work_dir + partic + "/ses-1/"
+            for folder in os.listdir(wd): 
+             #checks to make sure it's a participant
+
+                try:
+                    source = wd + folder
+                    print (source)
+                    if(folder == "beh"):
+                        print("beh")
+                        dest = "/projects/b1108/studies/rise/data/raw/neuroimaging/behavioral/"\
+                            + partic + "/ses-1/" + folder
+                        print(dest)
+                        shutil.copytree(source, dest) 
+                        print("Copied " + partic + " " + folder)
+                    else:
+                        print("other")
+                        dest = "/projects/b1108/studies/rise/data/raw/neuroimaging/bids/"\
+                             + partic + "/ses-1/" + folder
+                        print(dest)
+                        shutil.copytree(source, dest) 
+                        print("Copied " + partic + " " + folder)
+                except:
+                    print("Unable to copy " + partic)
+
+
+def main():   
+    mover()
+
+
+if __name__ == "__main__":
+    main()
