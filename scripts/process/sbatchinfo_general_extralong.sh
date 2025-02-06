841ffac91e1338f3a04b7c94a78d10970c84fadd
Ellyn Butler <ellynrbutler@gmail.com>
2025-02-06 12:45:19 -0600

    more mem

--- scripts/process/sbatchinfo_general_extralong.sh
+++ scripts/process/sbatchinfo_general_extralong.sh
@@ -4,5 +4,5 @@
 #SBATCH --nodes=1                                         ## how many computers do you need
 #SBATCH --ntasks-per-node=1                               ## how many cpus or processors do you need on each computer
 #SBATCH --time=120:00:00                                  ## how long does this need to run (remember different partitions have restrictions on this param)
-#SBATCH --mem-per-cpu=17G                                 ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --mem-per-cpu=25G                                 ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name="fmriprep"                             ## use the task id in the name of the job
